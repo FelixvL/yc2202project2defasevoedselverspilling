1b20d0a02bbebab28a0f183105e5af6aaa840daf
Deusie <43952614+Deusie@users.noreply.github.com>
2022-02-21 13:25:55 +0100

    Add aanvrager entiteit

---
+++ src/main/java/nl/ycvvapp/projectreal/domein/Aanvrager.java
@@ -0,0 +1,15 @@
+package nl.ycvvapp.projectreal.domein;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Aanvrager {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	long id;
+	
+	String titel;
+}
