1f108ff01fa2dae604b5ba259c4426d74b9ac924
FelixvL <digitaleservice@gmail.com>
2022-02-21 13:47:43 +0100

    repo fix

--- src/main/java/nl/ycvvapp/projectreal/persistence/AfhaalAfspraakRepository.java
+++ src/main/java/nl/ycvvapp/projectreal/persistence/AfhaalAfspraakRepository.java
@@ -1,5 +1,11 @@
 package nl.ycvvapp.projectreal.persistence;
 
-public interface AfhaalAfspraakRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
+
+import nl.ycvvapp.projectreal.domein.AfhaalAfspraak;
+
+@Component
+public interface AfhaalAfspraakRepository extends CrudRepository<AfhaalAfspraak, Long> {
 
 }
