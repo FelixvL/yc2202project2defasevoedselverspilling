813b38cfd4719300082665834bf3961a9f305dc4
Malin Elise Martinsen <malinelisemartinsen@Malins-MacBook-Pro.local>
2022-02-23 14:24:03 +0100

    post item

--- src/main/java/nl/ycvvapp/projectreal/persistence/ItemService.java
+++ src/main/java/nl/ycvvapp/projectreal/persistence/ItemService.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import nl.ycvvapp.projectreal.domein.Aanbieder;
 import nl.ycvvapp.projectreal.domein.AfhaalAfspraak;
 import nl.ycvvapp.projectreal.domein.Item;
 
@@ -13,24 +14,16 @@ import nl.ycvvapp.projectreal.domein.Item;
 public class ItemService {
 	@Autowired
 	ItemRepository ir;
-	
-	public void maakNieuwItem() {
-		Item item1 = new Item();
-		item1.setName("Bloemkool");
-		Item item2 = new Item();
-		item2.setName("Bloemkool");
-		Item item3 = new Item();
-		item3.setName("Bloemkool");
-		ir.save(item1);
-		ir.save(item2);
-		ir.save(item3);
-		System.out.println("Nieuwe items toegevoegd");
-	}
 
 	public Iterable<Item> go() {
 		 Iterable<Item> abcdef = ir.findAll();
 		 return abcdef;
 	}
+	
+	public void CreateItem(Item item) {
+		ir.save(item);
+	}
+	
 		
 }	
 
--- src/main/java/nl/ycvvapp/projectreal/rest/ItemEndpoint.java
+++ src/main/java/nl/ycvvapp/projectreal/rest/ItemEndpoint.java
@@ -2,9 +2,12 @@ package nl.ycvvapp.projectreal.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import nl.ycvvapp.projectreal.domein.Aanbieder;
 import nl.ycvvapp.projectreal.domein.AfhaalAfspraak;
 import nl.ycvvapp.projectreal.domein.Item;
 import nl.ycvvapp.projectreal.persistence.AfhaalAfspraakRepository;
@@ -16,16 +19,14 @@ public class ItemEndpoint {
 	@Autowired
 	ItemService is;
 	
-	@GetMapping ("/marianne/eerstemethode") 
-	public String eersteMethode() {
-		System.out.println("testmethode 1 werkt");
-		is.maakNieuwItem();
-		return "dit is de output van de eerste methode";
-	}
-	
 	@GetMapping("/marianne/getall")
 	public Iterable<Item> getmeallget() {	
 		return is.go();
+	}
+	
+	@PostMapping("/CreateItem")
+	public void createItem(@RequestBody Item item) {
+		is.CreateItem(item);
 	}
 	
 }
