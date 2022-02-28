5f057e5ebc382d379aec698b0734acafb6adf004
Deusie <43952614+Deusie@users.noreply.github.com>
2022-02-28 09:43:03 +0100

    ItemList aan user koppelen

--- src/main/java/nl/ycvvapp/projectreal/rest/ItemEndpoint.java
+++ src/main/java/nl/ycvvapp/projectreal/rest/ItemEndpoint.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import nl.ycvvapp.projectreal.domein.Item;
+import nl.ycvvapp.projectreal.domein.User;
 import nl.ycvvapp.projectreal.persistence.ItemService;
 
 @RestController
@@ -32,7 +33,7 @@ public class ItemEndpoint {
 	
 	
 	@PostMapping("/CreateItem")
-	public void createItem(@RequestBody Item item) {
+	public void createItem(@RequestBody Item item, User userid) {
 		is.CreateItem(item);
 	}
 
